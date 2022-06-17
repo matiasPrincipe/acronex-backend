f1135650620dc0ce414552569537f8a348cb497d
mprincipe <matiasprincipe@outlook.com>
2022-06-16 21:59:43 -0300

    Mejoras de codigo

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
     if (err) {
-        return console.log(`Error al conectarse a la base de datos: ${err}`)
+        console.log(`Error al conectarse a la base de datos: ${err}`)
+    } else {
+        console.log('Conexión a la bdd establecida');
     }
-    console.log('Conexión a la bdd establecida');
     app.listen(config.port, () => {
         console.log(`API REST corriendo en http://localhost:${config.port}`)
     })
